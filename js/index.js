12adf79456995b7dc6a288788806532a72f1d887
cyantis <cyantis@gmail.com>
2019-09-05 19:23:51 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,28 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a "g"');
+  }
+});
+
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+const divs = document.querySelectorAll('div');
+
+function goneBubblin(e) {
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i of divs) {
+  i.addEventListener('click', goneBubblin);
+}
